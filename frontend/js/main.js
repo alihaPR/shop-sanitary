40490fdfa19c72efd5f59824b33e0dcfd0dac2cc
alihaPR <alirezazand.me@gmail.com>
2026-06-20 10:21:00 +0330

    motion baner

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,17 @@
+new Swiper(".bannerSwiper",{
 
+    loop:true,
+
+    speed:800,
+
+    autoplay:{
+        delay:4000,
+        disableOnInteraction:false,
+    },
+
+    navigation:{
+        nextEl:".banner-next",
+        prevEl:".banner-prev",
+    }
+
+});
