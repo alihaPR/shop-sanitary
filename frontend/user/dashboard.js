0709ac68e11260e8110ddb445f5e1bce6e6b3845
alihaPR <alirezazand.me@gmail.com>
2026-07-25 00:35:20 +0330

    updat user panle 1

--- frontend/user/dashboard.js
+++ frontend/user/dashboard.js
@@ -22,38 +22,66 @@ async function loadDashboard() {
 
         document.getElementById("userName").innerText = data.userName;
 
-      
 
-const currentOrders =
-    data.pending +
-    data.processing +
-    data.shipped;
 
-document.getElementById("currentOrders").innerText =
-    currentOrders;
+        const currentOrders =
+            data.pending +
+            data.processing +
+            data.shipped;
 
-document.getElementById("delivered").innerText =
-    data.delivered;
+        document.getElementById("currentOrders").innerText =
+            currentOrders;
 
-document.getElementById("returned").innerText =
-    data.returned || 0;
+        document.getElementById("delivered").innerText =
+            data.delivered;
+
 
         document.getElementById("spent").innerText =
             data.totalSpent.toLocaleString() + " تومان";
 
         if (data.lastOrder) {
+const statusMap = {
+    pending: { text: "در انتظار بررسی", color: "#f59e0b" },
+    processing: { text: "در حال پردازش", color: "#3b82f6" },
+    shipped: { text: "ارسال شده", color: "#8b5cf6" },
+    delivered: { text: "تحویل شده", color: "#22c55e" },
+    cancelled: { text: "لغو شده", color: "#ef4444" }
+};
 
-            document.getElementById("lastOrderBox").innerHTML = `
+const status = statusMap[data.lastOrder.status] || {
+    text: data.lastOrder.status,
+    color: "#f8f4f4"
+};
 
-                <p><b>کد سفارش:</b> ${data.lastOrder._id.slice(-8)}</p>
+document.getElementById("lastOrderBox").innerHTML = `
 
-                <p><b>وضعیت:</b> ${data.lastOrder.status}</p>
+<div class="last-order-card">
 
-                <p><b>مبلغ:</b> ${data.lastOrder.totalPrice.toLocaleString()} تومان</p>
+    <div class="order-row">
+        <span>کد سفارش</span>
+        <strong>#${data.lastOrder._id.slice(-8)}</strong>
+    </div>
 
-                <p><b>تاریخ:</b> ${new Date(data.lastOrder.createdAt).toLocaleDateString("fa-IR")}</p>
+    <div class="order-row">
+        <span>وضعیت</span>
+        <span class="status-badge" id="status-badge-text"  style="background:${status.color}">
+            ${status.text}
+        </span>
+    </div>
 
-            `;
+    <div class="order-row">
+        <span>مبلغ</span>
+        <strong>${data.lastOrder.totalPrice.toLocaleString()} تومان</strong>
+    </div>
+
+    <div class="order-row">
+        <span>تاریخ</span>
+        <strong>${new Date(data.lastOrder.createdAt).toLocaleDateString("fa-IR")}</strong>
+    </div>
+
+</div>
+
+`;
 
         }
 
