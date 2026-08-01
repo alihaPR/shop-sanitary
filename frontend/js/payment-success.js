0ac96382b966dd02f74fe4782d35aa2e726bcb30
Hosseinbabaei1 <156106264+Hosseinbabaei1@users.noreply.github.com>
2026-08-01 15:38:17 +0330

    payment success final

---
+++ frontend/js/payment-success.js
@@ -0,0 +1,14 @@
+const params = new URLSearchParams(window.location.search);
+
+const refId = params.get("refId");
+
+const refElement = document.getElementById("refId");
+
+if (refId) {
+    refElement.innerHTML = `
+        <span>شماره پیگیری :</span>
+        <strong>${refId}</strong>
+    `;
+} else {
+    refElement.style.display = "none";
+}
